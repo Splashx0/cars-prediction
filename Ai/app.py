61cedd8fa5120ee79d5611f97726876cce0ec6f6
no one <72977120+Splashx0@users.noreply.github.com>
2024-06-06 02:50:30 +0100

    Update app.py

--- Ai/app.py
+++ Ai/app.py
@@ -2,25 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/health', methods=['GET'])
-def health_check():
-    try:
-        # Perform any necessary checks here (e.g., database connection check)
-        
-        # If everything is okay, return a success status
-        response = {
-            'status': 'healthy',
-            'message': 'The application is running smoothly.'
-        }
-        return jsonify(response), 200
-    except Exception as e:
-        # If there is an issue, return an error status
-        response = {
-            'status': 'unhealthy',
-            'message': str(e)
-        }
-        return jsonify(response), 500
-
+@app.route('/')
+def start():
+    return "HELLO FROM API"
 
 if __name__ == "__main__":
     app.run(debug=True)
