a5956d0c64bc5b66d01a8a4e20cd3313557965e4
no one <72977120+Splashx0@users.noreply.github.com>
2024-06-06 02:23:14 +0100

    Update app.py

--- Ai/app.py
+++ Ai/app.py
@@ -4,30 +4,6 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app)
-
-#@app.route('/')
-#def man():
-#    return render_template('home.html')
-
-@app.route('/test', methods=['POST'])
-def home():
-
-    json_data=request.get_json()
-    print(json_data)
-    car_dict={}
-
-    car_dict["marque"] = json_data['marque']
-    car_dict["modele"] =  json_data['modele']
-    car_dict["puissance_fiscale"] = int( json_data['puissance_fiscale'])
-    car_dict["kilometrage"] = int( json_data['kilometrage'])
-    car_dict["annee"] = int( json_data['annee'])
-    car_dict["energie"] =  json_data['energie']
-    car_dict["boite"] =  json_data['boite']
-
-    pred = model.prediction(car_dict)
-
-    return {"prediction":pred[0]}
 
 @app.route('/health', methods=['GET'])
 def health_check():
