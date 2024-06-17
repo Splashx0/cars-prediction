89345910ed88b918ca9e9f0954459c5fb143244d
Anes hadj khelifa <72977120+Splashx0@users.noreply.github.com>
2024-06-17 13:48:29 +0100

    Update app.py

--- Ai/app.py
+++ Ai/app.py
@@ -1,9 +1,30 @@
-from flask import Flask
-from handlers.routes import configure_routes
+from flask import Flask, render_template, request,jsonify
+import model
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-configure_routes(app)
+CORS(app)
+
+@app.route('/test', methods=['POST'])
+def home():
+
+    json_data=request.get_json()
+    print(json_data)
+    car_dict={}
+
+    car_dict["marque"] = json_data['marque']
+    car_dict["modele"] =  json_data['modele']
+    car_dict["puissance_fiscale"] = int( json_data['puissance_fiscale'])
+    car_dict["kilometrage"] = int( json_data['kilometrage'])
+    car_dict["annee"] = int( json_data['annee'])
+    car_dict["energie"] =  json_data['energie']
+    car_dict["boite"] =  json_data['boite']
+
+    pred = model.prediction(car_dict)
+
+    return {"prediction":pred[0]}
 
 if __name__ == "__main__":
     app.run(debug=True)
+
